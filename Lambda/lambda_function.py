213f2ad4fef5a941ffd2808f4df28284227d8ad2
Arthur Loukes <arthurloukes@gmail.com>
2025-03-17 15:11:27 +0000

    asd

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -9,10 +9,32 @@ log_level = os.environ.get('LOG_LEVEL', 'INFO')
 # Set the log level
 logger.setLevel(log_level)
 
+api_endpoints = ['/api/temperature',
+                 '/api/windspeed',
+                 '/api/humidity',
+                 '/api/pressure']
+
+def run_api_query(querystring):
+  if querystring.get('lon') and querystring.get('lat'):
+    response_content = json.dumps({'message':'You requested weather information for lon={}, lat={}'.format(querystring['lon'][0], querystring['lat'][0])})
+    response_code = '200 OK'
+  else:
+    response_content = json.dumps({'message':'You did not provide a valid lon and lat'})
+    response_code = '400 Bad Request'
+
+  response = {'body': response_content, 'code': response_code}
+  return response
+
 def lambda_handler(event, context):
     body = json.dumps(event['queryStringParameters'])
 
+    if event['path'] in api_endpoints:
+        response = run_api_query(event['queryStringParameters'])
+
     return {
-        'statusCode': 200,
-        'body': body
+        'statusCode': response['code'],
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': response['body']
     }
