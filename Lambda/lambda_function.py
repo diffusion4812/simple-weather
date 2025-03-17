d79ec58183bb2b21af3fa1be80d42c8dbbcbbf54
Arthur Loukes <arthurloukes@gmail.com>
2025-03-17 23:53:34 +0000

    asd

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,30 +1,49 @@
 import json, os, logging, boto3
 from boto3.dynamodb.conditions import Key, Attr
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return super().default(obj)
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('wx-serv-data')
+data_table = dynamodb.Table('wx-serv-data')
+stations_table = dynamodb.Table('wx-serv-stations')
+
+lambda_populate = boto3.client('lambda')
 
 logger = logging.getLogger()
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
 logger.setLevel(log_level)
 
-def get_temperature(lon, lat):
-    response_content = json.dumps({'temperature': str("{:0.5f}".format(25.0))})
+def get_record(station_id):
+    response = data_table.query(
+        KeyConditionExpression=Key('station_id').eq(station_id),
+        ScanIndexForward=False,
+        Limit=1
+    )
+    return json.loads(json.dumps(response['Items'], cls=DecimalEncoder))
+
+def get_temperature(station_id):
+    response_content = json.dumps({'temperature': get_record(station_id)[0]['temperature']})
     response_code = '200'
     return response_content, response_code
 
-def get_windspeed(lon, lat):
-    response_content = json.dumps({'message': 'valid longitude and latitude provided'})
+def get_windspeed(station_id):
+    response_content = json.dumps({'speed': get_record(station_id)[0]['windspeed']['speed'],
+                                   'bearing': get_record(station_id)[0]['windspeed']['bearing']})
     response_code = '200'
     return response_content, response_code
 
-def get_humidity(lon, lat):
-    response_content = json.dumps({'message': 'valid longitude and latitude provided'})
+def get_humidity(station_id):
+    response_content = json.dumps({'temperature': get_record(station_id)[0]['humidity']})
     response_code = '200'
     return response_content, response_code
 
-def get_pressure(lon, lat):
-    response_content = json.dumps({'message': 'valid longitude and latitude provided'})
+def get_pressure(station_id):
+    response_content = json.dumps({'temperature': get_record(station_id)[0]['pressure']})
     response_code = '200'
     return response_content, response_code
 
@@ -34,10 +53,10 @@ api_endpoints = {'/api/temperature': get_temperature,
                  '/api/pressure': get_pressure}
 
 def run_api_query(path, querystring):
-    if querystring.get('lon') and querystring.get('lat'):
-        response_content, response_code = api_endpoints[path](querystring['lon'], querystring['lat'])
+    if querystring.get('station_id'):
+        response_content, response_code = api_endpoints[path](querystring['station_id'])
     else:
-        response_content = json.dumps({'message': 'valid longitude and latitude not provided'})
+        response_content = json.dumps({'message': 'valid station ID required'})
         response_code = '400'
 
     response = {'body': response_content, 'code': response_code}
@@ -57,12 +76,9 @@ def lambda_handler(event, context):
             'code': '400'
         }
 
-    results = table.query(
-        KeyConditionExpression=Key('station_id').eq('ZVDB')
-    )
-    items = results.items()
-
-    response['body'] = json.dumps(items)
+    lambda_populate.invoke(FunctionName="wx-serv-populate",
+                            InvocationType='Event',
+                            Payload='{}')
 
     return {
         'isBase64Encoded': False,
